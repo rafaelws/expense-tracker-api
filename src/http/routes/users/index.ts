e5b693e327849decfa106b6e99d6c5f72561b7f2
rafaelws <420753+rafaelws@users.noreply.github.com>
2025-06-24 17:31:28 -0300

    refac: HTTP layer abstraction #3

--- src/http/routes/users/index.ts
+++ src/http/routes/users/index.ts
@@ -1,19 +1,10 @@
 import { Router } from "express";
 
-import {
-  authenticateUserSchema,
-  createUserSchema,
-} from "@/features/users/user-schema";
-import { ensureBodySchema } from "@/http/middlewares/ensure-schema-middleware";
+import { httpRoute } from "@/http/lib/adapter";
 
 import { postAuthenticate, postUser } from "./user-http-handlers";
 
 export const usersRouter = Router();
 
-usersRouter.post("/users", ensureBodySchema(createUserSchema), postUser);
-
-usersRouter.post(
-  "/auth",
-  ensureBodySchema(authenticateUserSchema),
-  postAuthenticate,
-);
+usersRouter.post("/users", httpRoute(postUser));
+usersRouter.post("/auth", httpRoute(postAuthenticate));
--- src/http/routes/users/user-http-handlers.ts
+++ src/http/routes/users/user-http-handlers.ts
@@ -1,36 +1,29 @@
-import { Response } from "express";
-
 import { UserRepository } from "@/features/users/user-repository";
 import {
-  AuthenticateUserDTO,
-  CreateUserDTO,
+  authenticateUserSchema,
+  createUserSchema,
 } from "@/features/users/user-schema";
 import { UserService } from "@/features/users/user-service";
 import { jwt } from "@/http/lib/jwt";
-import { HandlerRequest } from "@/http/lib/types";
+import { HttpRequest, reply } from "@/http/lib/types";
+import { validate } from "@/http/lib/validate";
 
 const userService = new UserService(new UserRepository());
 
-export async function postUser(
-  req: HandlerRequest<CreateUserDTO>,
-  res: Response,
-) {
-  const result = await userService.createUser(req.body);
+export async function postUser({ body }: HttpRequest) {
+  const dto = validate(body, createUserSchema);
+  const result = await userService.createUser(dto);
   if (result === null) {
-    res.status(400).json({ message: "Invalid e-mail or password." });
-    return;
+    return reply(400, { message: "Invalid e-mail or password." });
   }
-  res.status(201).json({ token: jwt.sign(result.id) });
+  return reply(201, { token: jwt.sign(result.id) });
 }
 
-export async function postAuthenticate(
-  req: HandlerRequest<AuthenticateUserDTO>,
-  res: Response,
-) {
-  const result = await userService.authenticateUser(req.body);
+export async function postAuthenticate({ body }: HttpRequest) {
+  const dto = validate(body, authenticateUserSchema);
+  const result = await userService.authenticateUser(dto);
   if (result === null) {
-    res.status(401).json({ message: "Invalid e-mail or password." });
-    return;
+    return reply(401, { message: "Invalid e-mail or password." });
   }
-  res.status(200).json({ token: jwt.sign(result.id) });
+  return reply(200, { token: jwt.sign(result.id) });
 }
